cfbc0062671dcfda08eed53a0602e66aa2cbd9a8
KamesAI <kalcmspro@gmail.com>
2025-11-09 16:47:00 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,35 @@
-/**** Next.js config ****/
-import type { NextConfig } from 'next'
+import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    typedRoutes: true,
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
   },
-} 
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
-export default nextConfig
+export default nextConfig;
